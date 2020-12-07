0883ef0933b7b7d4d55afab9e30c8c8af7963ae6
Hrishabh-Punetha <60377183+Hrishabh-Punetha@users.noreply.github.com>
2020-12-07 23:40:33 +0530

    Final Commit

--- Input/main.c
+++ Input/main.c
@@ -6,8 +6,8 @@
 
 void main()
 {
-        int ** difference_matrix=diff_rssi(10,5);
-        for(int i=0;i<10;i++)
+        int ** difference_matrix=diff_rssi(5,5);
+        for(int i=0;i<5;i++)
         {
             for(int j=0;j<5;j++)
             {
