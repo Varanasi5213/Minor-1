51d282ae0d0fa33c0cd1fb8194381611534daad1
Hrishabh-Punetha <60377183+Hrishabh-Punetha@users.noreply.github.com>
2020-12-07 23:39:48 +0530

    Final Commit

--- Input/main.c
+++ Input/main.c
@@ -1,12 +1,22 @@
+// Including the required header files
 #include<stdio.h>
+#include<stdlib.h>
 #include "DifferenceMatrix.h"
 
 
 void main()
 {
-        int diff_matrix=diff_rssi(5,5);
-         printf("\n",diff_matrix);
+        int ** difference_matrix=diff_rssi(10,5);
+        for(int i=0;i<10;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                printf("%d ",difference_matrix[i][j]);      // Printing the difference matrix
 
+            }
+               printf("\n");
+
+        }
 
 }
 
