e35ba43138088c8a823351fd5b3810dc7d10aa6a
Hrishabh-Punetha <60377183+Hrishabh-Punetha@users.noreply.github.com>
2020-12-08 15:18:43 +0530

    Final Commit

--- Input/main.c
+++ Input/main.c
@@ -1,12 +1,11 @@
 // Including the required header files
 #include<stdio.h>
-#include<stdlib.h>
 #include "DifferenceMatrix.h"
 
 
 void main()
 {
-        int ** difference_matrix=diff_rssi(5,5);
+         int ** difference_matrix=diff_rssi(5,5);
         for(int i=0;i<5;i++)
         {
             for(int j=0;j<5;j++)
@@ -19,5 +18,6 @@ void main()
         }
 
 }
+
 
 
